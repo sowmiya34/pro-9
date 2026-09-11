8fd34119807f7da012e7d826085380208dcd5456
sowmiya r <88027669+sowmiya34@users.noreply.github.com>
2026-09-11 10:42:43 +0530

    Update firewall_config.sh

--- starter/firewall_config.sh
+++ starter/firewall_config.sh
@@ -22,26 +22,4 @@
 
 
 exit 0
-#!/bin/bash
 
-# Port-Based Firewall Configuration
-# Complete the commands below.
-
-# TODO 1: Open TCP port 8080
-firewall-cmd --add-port=8080/tcp
-
-# TODO 2: Open TCP port 9000
-firewall-cmd --add-port=9000/tcp
-
-# TODO 3:md --list-ports
-
-# TODO 4: Remove TCP port 8080
-firewall-cmd --remove-port=8080/tcp
-
-# TODO 5: Permanently open TCP port 3000
-firewall-cmd --add-port=3000/tcp --permanent
-
-# TODO 6: Reload the firewall
-firewall-cmd --reload
-
-exit 0
